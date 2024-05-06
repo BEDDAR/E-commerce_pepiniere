1c168fd03d5ec37192062860d9b89d4780342494
BEDDAR <beddarrebiha@gmail.com>
2024-05-06 16:54:02 +0200

    multer

--- backEnd/routes/produitsRouter.js
+++ backEnd/routes/produitsRouter.js
@@ -1,13 +1,18 @@
 const produitController = require('../controllers/produitsController.js')
-const multer  = require('multer');
+const multer = require('multer');
 
 // Configuration de Multer pour gérer les fichiers téléchargés
 const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: {
-      fileSize: 10 * 1024 * 1024, // Limite de taille maximale de 5 Mo
-    },
-  }); // Stocke l'image en mémoire
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // Limite de taille maximale de fichier de 5 Mo
+    // Limite de taille totale des données en mémoire
+    // Assurez-vous que cette limite est sécuritaire par rapport à la capacité de votre serveur
+    // Par exemple, si vous avez 1 Go de mémoire disponible, vous pourriez définir la limite à 100 * 1024 * 1024 (100 Mo)
+    // Pour une sécurité accrue, vous pouvez définir une limite encore plus basse.
+    contentLength: 100 * 1024 * 1024, // Limite de taille totale des données en mémoire (100 Mo)
+  },
+});
 
 const router = require('express').Router()
 
@@ -19,6 +24,6 @@ router.get('/potager', produitController.getPotager)
 router.get('/materiel', produitController.getMateriel)
 router.get('/soins', produitController.getSoins)
 router.patch('/updatestock', produitController.updateStock)
-router.post('/ajoutproduit',upload.single('imageProduit'), produitController.ajoutProduit)
+router.post('/ajoutproduit', upload.single('imageProduit'), produitController.ajoutProduit)
 
 module.exports = router
