013bdbfdfec10c70d924e3fea0eb86b79b1fe12d
BEDDAR <beddarrebiha@gmail.com>
2024-05-07 18:12:50 +0200

    màj de tests

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -9,36 +9,4 @@ describe('Express App', () => {
     expect(response.body).toEqual({ message: 'Hello' });
   });
 
-  // Test de l'utilisation du middleware bodyParser.json()
-  test('POST with JSON data', async () => {
-    const data = { name: 'John Doe', age: 30 };
-    const response = await request(app)
-      .post('/')
-      .send(data)
-      .set('Content-Type', 'application/json');
-    expect(response.statusCode).toBe(200);
-    // Ajoutez d'autres assertions si nécessaire
-  });
-
-  // Test de l'utilisation du middleware bodyParser.urlencoded()
-  test('POST with URL encoded data', async () => {
-    const data = new URLSearchParams();
-    data.append('name', 'John Doe');
-    data.append('age', '30');
-    const response = await request(app)
-      .post('/')
-      .send(data.toString())
-      .set('Content-Type', 'application/x-www-form-urlencoded');
-    expect(response.statusCode).toBe(200);
-    // Ajoutez d'autres assertions si nécessaire
-  });
-
-  // Test de l'utilisation du middleware cookieParser()
-  test('GET with cookie', async () => {
-    const response = await request(app)
-      .get('/')
-      .set('Cookie', ['token=abcdefg']);
-    expect(response.statusCode).toBe(200);
-    // Ajoutez d'autres assertions si nécessaire
-  });
 });
