a3de5146e1eaf5516fc5221c6046f7bafd27c1da
BEDDAR <beddarrebiha@gmail.com>
2024-05-07 15:08:37 +0200

    app sécurisée

---
+++ backEnd/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const app = express();
+
+
+
+//Middleware
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.json())
+app.use(cors({ origin: true, credentials: true }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser())
+//Routers
+
+const routerProduits=require('./routes/produitsRouter.js')
+app.use('/',routerProduits)
+
+const routerNoteAvis=require('./routes/noteAvisRouter.js')
+app.use('/',routerNoteAvis)
+
+const routerUtilisateurs=require('./routes/utilisateursRouter.js')
+app.use('/',routerUtilisateurs)
+
+const routerCommandes=require('./routes/commandeRouter.js')
+app.use('/',routerCommandes)
+
+const routerContenuCommandes=require('./routes/contenuCommandeRouter.js')
+app.use('/',routerContenuCommandes)
+
+app.get('/',(req,res)=>{
+    res.json('{"message":"Hello"}')
+})
+
+module.exports=app;
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,37 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const cookieParser = require('cookie-parser')
-const app = express();
+const app =require('./app')
 const port = 3000; 
-
-
-//Middleware
-
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(express.json())
-app.use(cors({ origin: true, credentials: true }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(cookieParser())
-//Routers
-
-const routerProduits=require('./routes/produitsRouter.js')
-app.use('/',routerProduits)
-
-const routerNoteAvis=require('./routes/noteAvisRouter.js')
-app.use('/',routerNoteAvis)
-
-const routerUtilisateurs=require('./routes/utilisateursRouter.js')
-app.use('/',routerUtilisateurs)
-
-const routerCommandes=require('./routes/commandeRouter.js')
-app.use('/',routerCommandes)
-
-const routerContenuCommandes=require('./routes/contenuCommandeRouter.js')
-app.use('/',routerContenuCommandes)
-
-app.get('/',(req,res)=>{
-    res.json('{"message":"Hello"}')
-})
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
