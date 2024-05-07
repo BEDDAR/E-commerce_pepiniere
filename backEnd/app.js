02efa8dd88b990e076ba744d392242eebd3d216b
BEDDAR <beddarrebiha@gmail.com>
2024-05-07 16:36:32 +0200

    maj

--- backEnd/app.js
+++ backEnd/app.js
@@ -8,7 +8,6 @@ const app = express();
 app.disable('x-powered-by');
 
 //Middleware
-
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json())
 app.use(cors({ origin: true, credentials: true }))
