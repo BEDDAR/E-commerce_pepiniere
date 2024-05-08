9e1e27e00b27d6a63240cdf1b9b71361263423da
BEDDAR <beddarrebiha@gmail.com>
2024-05-08 18:27:53 +0200

    multer

--- backEnd/routes/produitsRouter.js
+++ backEnd/routes/produitsRouter.js
@@ -1,6 +1,24 @@
 const produitController = require('../controllers/produitsController.js')
+const multer = require('multer');
 
+// Configuration de Multer
+const storage = multer.memoryStorage();
 
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // Limite de taille maximale de fichier de 5 Mo
+  },
+  fileFilter: (req, file, cb) => {
+    // Validation du type de fichier
+    const allowedMimes = ['image/jpeg', 'image/png'];
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Type de fichier non pris en charge'));
+    }
+  },
+});
 
 const router = require('express').Router()
 
@@ -12,6 +30,6 @@ router.get('/potager', produitController.getPotager)
 router.get('/materiel', produitController.getMateriel)
 router.get('/soins', produitController.getSoins)
 router.patch('/updatestock', produitController.updateStock)
-router.post('/ajoutproduit', produitController.ajoutProduit)
+router.post('/ajoutproduit',upload.single('imageProduit'), produitController.ajoutProduit)
 
 module.exports = router
