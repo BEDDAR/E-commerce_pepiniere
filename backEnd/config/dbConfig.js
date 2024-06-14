32b2bd56b19d5e1fc2b485893ed0944459babad5
BEDDAR <beddarrebiha@gmail.com>
2024-06-14 15:43:12 +0200

    mysql config

--- backEnd/config/dbConfig.js
+++ backEnd/config/dbConfig.js
@@ -3,6 +3,5 @@ HOST:'localhost',
 USER:'root',
 PASSWORD:'',
 DB:'projet',
-dialect:'mysql',
-socketPath:'/tmp/mysql.sock'
+dialect:'mysql'
 }
--- backEnd/models/index.js
+++ backEnd/models/index.js
@@ -10,8 +10,7 @@ const sequelize = new Sequelize(
     dbConfig.USER,       // Nom d'utilisateur de la base de données
     dbConfig.PASSWORD,   // Mot de passe de la base de données
     {
-        host: dbConfig.HOST,
-        socketpth: dbConfig.socketPath, // Adresse du serveur de la base de données
+        host: dbConfig.HOST, // Adresse du serveur de la base de données
         dialect: dbConfig.dialect // Type de dialecte de la base de données (par exemple, 'mysql', 'postgres', etc.)
     }
 );
