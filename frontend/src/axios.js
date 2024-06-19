291d704bcf10d4481e2ef6a4ad904ea313922849
BEDDAR <beddarrebiha@gmail.com>
2024-06-19 18:36:03 +0200

    a

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-axios.defaults.baseURL= "https://ec2-51-20-32-174.eu-north-1.compute.amazonaws.com/" || 'http://localhost:3000/';
+axios.defaults.baseURL= "http://ec2-51-20-32-174.eu-north-1.compute.amazonaws.com/" || 'http://localhost:3000/';
