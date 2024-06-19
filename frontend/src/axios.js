982d81b1cb4edcde2b200478d85282735d739bfe
BEDDAR <beddarrebiha@gmail.com>
2024-06-19 20:06:44 +0200

    env

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,7 @@ import axioss from 'axios'
 
 let baseURL;
 
-baseURL = process.env.NODE_ENV==='production'?'http://ec2-51-20-32-174.eu-north-1.compute.amazonaws.com/':'http://localhost:3000/'
+baseURL = process.env.NODE_ENV==='production'?'https://ec2-51-20-32-174.eu-north-1.compute.amazonaws.com/':'http://localhost:3000/'
 const axios = axioss.create({
 baseURL: baseURL,
 })
